75e67e970fa6d0b7a2fae2f0fbcec62e3514af55
Anatoly <phonon19@rambler.ru>
2021-02-02 20:15:54 +0400

    Make class from the function

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-const numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?","");
+'use strict';
 
-const personalMovieDB={
-    count: numberOfFilms,
+
+const personalMovieDB = {
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы цже посмотрели?',");
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Один из просмотренных фильмов?", ""),
+                b = prompt("На сколько оцените его?", "");
+            if (a != null && b != null && a != '' & b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');;
+            } else {
+                console.log('eror');
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классичемкий зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+
+    showMyDB: function(hidden){
+        if(!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    
+    writeyourGenres: function(){
+        for(let i=1;i<=3;i++){
+            personalMovieDB.genres[i-1]=prompt(`Ваш любимый жанр под номером ${i}`);
+        }
+    }
+    
 };
 
-const a=prompt("Один из просмотренных фильмов?",""),
-      b=prompt("На сколько оцените его?",""),
-      c=prompt("Один из просмотренных фильмов?",""),
-      d=prompt("На сколько оцените его?","");
-personalMovieDB.movies[a]=b;   
-personalMovieDB.movies[c]=b;
 
- console.log(personalMovieDB);
